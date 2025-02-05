b4996635fd9c7a9062c819e4a289adb1609e1f62
TaitoYoshimura <pz6f9vej@s.okayama-u.ac.jp>
2025-02-05 13:55:57 +0900

    fix convertImageToTensorBuffer

--- lib/src/image/image_conversions.dart
+++ lib/src/image/image_conversions.dart
@@ -58,31 +58,36 @@ class ImageConversions {
   static void convertImageToTensorBuffer(Image image, TensorBuffer buffer) {
     int w = image.width;
     int h = image.height;
-    List<int> intValues = image.data as List<int>;
+
+    Uint8List bytes = image.getBytes();
     int flatSize = w * h * 3;
     List<int> shape = [h, w, 3];
+
     switch (buffer.getDataType()) {
       case TfLiteType.uint8:
         List<int> byteArr = List.filled(flatSize, 0);
-        for (int i = 0, j = 0; i < intValues.length; i++) {
-          byteArr[j++] = ((intValues[i]) & 0xFF);
-          byteArr[j++] = ((intValues[i] >> 8) & 0xFF);
-          byteArr[j++] = ((intValues[i] >> 16) & 0xFF);
+        for (int i = 0, j = 0; i < bytes.length; i += 4) { // 4バイト単位 (RGBA)
+          byteArr[j++] = bytes[i];     // R
+          byteArr[j++] = bytes[i + 1]; // G
+          byteArr[j++] = bytes[i + 2]; // B
+          // bytes[i + 3] (A) は無視
         }
         buffer.loadList(byteArr, shape: shape);
         break;
+
       case TfLiteType.float32:
         List<double> floatArr = List.filled(flatSize, 0.0);
-        for (int i = 0, j = 0; i < intValues.length; i++) {
-          floatArr[j++] = ((intValues[i]) & 0xFF).toDouble();
-          floatArr[j++] = ((intValues[i] >> 8) & 0xFF).toDouble();
-          floatArr[j++] = ((intValues[i] >> 16) & 0xFF).toDouble();
+        for (int i = 0, j = 0; i < bytes.length; i += 4) { // 4バイト単位 (RGBA)
+          floatArr[j++] = bytes[i].toDouble();     // R
+          floatArr[j++] = bytes[i + 1].toDouble(); // G
+          floatArr[j++] = bytes[i + 2].toDouble(); // B
         }
         buffer.loadList(floatArr, shape: shape);
+        print("floatArr : ${floatArr}");
         break;
+
       default:
-        throw StateError(
-            "${buffer.getDataType()} is unsupported with TensorBuffer.");
+        throw StateError("${buffer.getDataType()} is unsupported with TensorBuffer.");
     }
   }
 }
